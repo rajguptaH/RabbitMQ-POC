b25b3268862f1618647e5931958b36f542d0a800
rajguptaH <rajkumar00999.rk@gmail.com>
2024-02-14 21:54:23 +0530

    Add project files.

---
+++ CunsumerRabbitMQ/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Cunsumer!");
+
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+var cunsumer = new EventingBasicConsumer(channel);
+cunsumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var result = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Incoming Message: " + result);
+};
+channel.BasicConsume("main_queue",true,cunsumer);
+Console.ReadLine();
---
+++ Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Print();
+    }
+    public static void Print()
+    {
+        Console.WriteLine("Want To Send Message Choose Y/Yes!");
+        var req = Console.ReadLine();
+        if (req.ToLower() == "y" || req.ToLower() == "yes")
+        {
+            Console.WriteLine("Enter Message!");
+            var message = Console.ReadLine();
+            Send(message);
+        }
+        else
+        {
+            Print();
+        }
+    }
+    public static void Send(string message)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.BasicPublish("main", "main_key", null, Encoding.UTF8.GetBytes(message));
+        Console.WriteLine("Message Sent Sucessfully!");
+        Print();
+    }
+}
